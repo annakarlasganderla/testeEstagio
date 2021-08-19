515238f31f2961045eeb598bdab6b63c5aeee662
Anna Karla Sganderla <sganderlakarlaanna@gmail.com>
2021-08-19 15:22:39 -0300

    add files

---
+++ main.js
@@ -0,0 +1,19 @@
+function abreCadastra() {
+    window.location.replace("cadastro.html")
+}
+
+function finalizaCad() {
+    window.location.replace("login.html")
+}
+
+function volta() {
+    window.location.replace("login.html")
+}
+
+function novaSenha() {
+    window.location.replace("nova-senha.html")
+}
+
+function finalizaSenha() {
+    window.location.replace("login.html")
+}
